b99d2a582f07f53f6b2609abd819a682e6017ebd
Hugo Roca <hugo.rock20@hotmail.com>
2018-04-23 11:49:12 -0500

    cambio

--- QY_ExistsStoreOrFunction.sql
+++ QY_ExistsStoreOrFunction.sql
@@ -2,3 +2,9 @@ IF EXISTS(SELECT * FROM sys.SYSOBJECTS A INNER JOIN SYS.SCHEMAS B ON A.uid =B.sc
     WHERE A.NAME='Gen_SelBandejaProducto_SP' AND B.name='dbo')
  DROP PROCEDURE Gen_SelBandejaProducto_SP 
 GO
+
+-- para indeces 
+IF EXISTS (SELECT * FROM sys.indexes WHERE name='Cedula_INDEX' AND object_id = OBJECT_ID('CertificadoTributarioDetalle'))
+BEGIN
+	DROP INDEX Cedula_INDEX ON dbo.CertificadoTributarioDetalle; 
+END

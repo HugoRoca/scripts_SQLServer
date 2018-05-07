4b802bc7c9b51dd47c32e089ffd7b42afd243343
Hugo Roca <hugo.rock20@hotmail.com>
2018-05-07 10:56:20 -0500

    correción QY_buscarTablaEnSP

--- QY_buscarTablaEnSP.sql
+++ QY_buscarTablaEnSP.sql
@@ -1,3 +1,18 @@
 SELECT Name
 FROM sys.procedures
 WHERE OBJECT_DEFINITION(OBJECT_ID) LIKE '%table%'
+Go
+
+------
+SELECT DISTINCT so.name
+FROM syscomments sc
+INNER JOIN sysobjects so ON sc.id=so.id
+WHERE sc.TEXT LIKE '%table%'
+Go
+
+----- Con tipo, si es store o function
+SELECT DISTINCT o.name, o.xtype
+FROM syscomments c
+INNER JOIN sysobjects o ON c.id=o.id
+WHERE c.TEXT LIKE '%SolicitudCredito%'
+go
